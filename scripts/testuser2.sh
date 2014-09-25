0974d1ad0fcaf50761d80398c9be4b94f4aa7141
Michael Andrew <michael@gladeye.co.nz>
2014-09-26 10:56:44 +1200

    Added automated test user scripts to make it easier to log back in users and test changes to server code since server must be restarted each time changes are made to Session.js

---
+++ scripts/testuser2.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/expect -f
+
+#
+# Test user script 2
+#
+# A test user script for JanusVR server running on local machine on default JanusVR server port.
+#
+# Automatically logs in a user to room eab63d0ea060b828578a4ae044f24d03 called testuser2 and subscribes for events from
+# server. After doing this it allows interactive mode so any other command can be entered and sent by the developer on
+# behalf of this user.
+#
+# @author Michael Andrew (michael@uxvirtual.com)
+#
+
+spawn telnet 127.0.0.1 5566
+send "{\"method\":\"logon\",\"data\":{\"userId\":\"testuser2\", \"version\":\"28.9\", \"roomId\":\"eab63d0ea060b828578a4ae044f24d03\"}}\n"
+send "{\"method\":\"subscribe\", \"data\":{\"roomId\":\"eab63d0ea060b828578a4ae044f24d03\"}}\n"
+send "{\"method\":\"enter_room\", \"data\":{\"roomId\":\"eab63d0ea060b828578a4ae044f24d03\"}}\n"
+interact #This hands control of the keyboard over to you
